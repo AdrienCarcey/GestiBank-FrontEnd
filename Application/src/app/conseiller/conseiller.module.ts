a2a660d823f7e8bc19b69999cba2fffc878fba83
AdrienCarcey <adrien.carcey@hotmail.fr>
2018-06-20 13:38:50 +0200

    Re-initialisation du projet

---
+++ Application/src/app/conseiller/conseiller.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConseillerRoutingModule } from './conseiller-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ConseillerRoutingModule
+  ],
+  declarations: []
+})
+export class ConseillerModule { }
