e575e3a3fa27ed4c4c6e2287e2ad325c9c90b137
Claire <claire.aly1@gmail.com>
2018-06-20 16:12:44 +0200

    Modele V2

--- Application/src/app/modele/compte-remunere.ts
+++ Application/src/app/modele/compte-remunere.ts
@@ -1,10 +1,16 @@
-export class CompteRemunere {
+import { Transaction } from './Transaction'
+import { Compte } from './Compte'
+
+export class CompteRemunere extends Compte {
 
 	txRemuneration: number;
 
 
-	constructor(txRemuneration: number){
+	constructor(numCompte: number, dateCreation: Date, solde: number, 
+		montantDecouvert: number, historique: Array<Transaction>, 
+		etatCompte: string, txRemuneration: number){
 		super(numCompte, dateCreation, solde, montantDecouvert,
-			historique, enum etatCompte);
+			historique, etatCompte);
+		this.txRemuneration = txRemuneration;
 	}
 }
--- Application/src/app/modele/compte.ts
+++ Application/src/app/modele/compte.ts
@@ -8,4 +8,16 @@ export class Compte {
 	montantDecouvert: number;
 	historique: Array<Transaction>;
 	etatCompte: string; // valide ou invalide, à redefinir avec une enumeration
+
+	constructor(numCompte: number, dateCreation: Date, solde: number, 
+		montantDecouvert: number, historique: Array<Transaction>, 
+		etatCompte: string){
+
+		this.numCompte = numCompte;
+		this.dateCreation = dateCreation;
+		this.solde = solde;
+		this.montantDecouvert = montantDecouvert;
+		this.historique = historique;
+		this.etatCompte = etatCompte;
+	}
 }
