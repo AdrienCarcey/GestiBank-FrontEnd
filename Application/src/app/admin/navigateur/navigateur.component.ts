6bac4142e45554a49110835493efccd42389fc76
MahtieuPOEI <mathieu.chastenet@gmail.com>
2018-06-24 18:18:47 +0200

    Nouvelle version nav module admin

--- Application/src/app/admin/navigateur/navigateur.component.ts
+++ Application/src/app/admin/navigateur/navigateur.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigateur',
   templateUrl: './navigateur.component.html',
-  styleUrls: ['./navigateur.component.css']
+  styleUrls: ['./navigateur.component.css'],
 })
+
+
+
 export class NavigateurComponent implements OnInit {
 
-  constructor() { }
+route: string;
+
+  constructor(location: Location, router: Router) {
+    router.events.subscribe((val) => {
+      if(location.path() != ''){
+        this.route = location.path();
+      } else {
+        this.route = '/admin'
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
-}
+} // fin classe NavigateurComponent
+
