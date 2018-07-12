8d241d0cb0afd38672d76f68c927abaa2ec01780
Adrien CARCEY <Adrien@MacBook-Air-de-Adrien.local>
2018-07-12 09:21:55 +0200

    Amelioration Web Service

--- Application/src/app/test/adrien/adrien.component.ts
+++ Application/src/app/test/adrien/adrien.component.ts
@@ -13,13 +13,20 @@ import { ClientTestService } from "../../services/clientTest.service";
 })
 
 export class AdrienComponent implements OnInit {
-	clientsInterne: Array<ClientTest>;
-  clientInterne: ClientTest = new ClientTest();
+	clientsInterne = new Array<ClientTest>();
+  clientInterne = new ClientTest();
   createClientInterne: boolean;
   deleteClientInterne: boolean;
-  updateClientInterne: boolean
+  updateClientInterne: boolean;
   createCompteInterne: boolean;
   deleteCompteInterne: boolean;
+  updateCompteInterne: boolean;
+
+  buttonModifierClient: String;
+  buttonAnnulerClient: String;
+  buttonSupprimerClient: String;
+  buttonEnregistrerClient: String;
+  formCreationClient:String;
 
   clientForm = new FormGroup ({
     updateFirstName: new FormControl(),
@@ -31,9 +38,18 @@ export class AdrienComponent implements OnInit {
     createLastName: new FormControl(),
     createEmail: new FormControl(),
     createMobile: new FormControl()
-  });
+  })
+
+  buttonModifierCompte: String;
+  buttonAnnulerCompte: String;
+  buttonSupprimerCompte: String;
+  buttonEnregistrerCompte: String;
+  formCreationCompte:String;
 
   compteForm = new FormGroup ({
+    updateDescription: new FormControl(),
+    updateSolde: new FormControl(),
+
     createIdClient: new FormControl(),
     createDescription: new FormControl(),
     createSolde: new FormControl()
@@ -42,6 +58,26 @@ export class AdrienComponent implements OnInit {
 	constructor(private clientTestService: ClientTestService) {}
 
 	ngOnInit() {
+    this.buttonModifierClient = "visible";
+    this.buttonAnnulerClient = "invisible";
+    this.buttonSupprimerClient = "visible";
+    this.buttonEnregistrerClient = "invisible";
+    this.formCreationClient = "visible";
+
+    this.clientForm.controls['updateFirstName'].disable();
+    this.clientForm.controls['updateLastName'].disable();
+    this.clientForm.controls['updateEmail'].disable();
+    this.clientForm.controls['updateMobile'].disable();
+    
+    this.buttonModifierCompte = "visible";
+    this.buttonAnnulerCompte = "invisible";
+    this.buttonSupprimerCompte = "visible";
+    this.buttonEnregistrerCompte = "invisible";
+    this.formCreationCompte = "visible";
+    
+    this.compteForm.controls['updateDescription'].disable();
+    this.compteForm.controls['updateSolde'].disable();
+
     this.clientTestService.findAllClients().subscribe(
         clientsResponse => {
           this.clientsInterne = clientsResponse;
@@ -146,5 +182,70 @@ export class AdrienComponent implements OnInit {
       );
 
     window.location.reload();
+  }
+
+  updateCompte(idCompte: number) {
+    let compte = new CompteTest();
+    compte.description = this.compteForm.controls['updateDescription'].value;
+    compte.solde = this.compteForm.controls['updateSolde'].value;
+
+    this.clientTestService.updateCompteById(idCompte, compte).subscribe(
+        updateResponse => {
+          this.updateCompteInterne = updateResponse;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+
+    window.location.reload();
+  }
+
+  modifierClient() {
+    this.buttonModifierClient = "invisible";
+    this.buttonAnnulerClient = "visible";
+    this.buttonSupprimerClient = "invisible";
+    this.buttonEnregistrerClient = "visible";
+    this.formCreationClient = "invisible";
+
+    this.clientForm.controls['updateFirstName'].enable();
+    this.clientForm.controls['updateLastName'].enable();
+    this.clientForm.controls['updateEmail'].enable();
+    this.clientForm.controls['updateMobile'].enable();
+  }
+
+  annulerClient() {
+    this.buttonModifierClient = "visible";
+    this.buttonAnnulerClient = "invisible";
+    this.buttonSupprimerClient = "visible";
+    this.buttonEnregistrerClient = "invisible";
+    this.formCreationClient = "visible";
+
+    this.clientForm.controls['updateFirstName'].disable();
+    this.clientForm.controls['updateLastName'].disable();
+    this.clientForm.controls['updateEmail'].disable();
+    this.clientForm.controls['updateMobile'].disable();
+  }
+
+   modifierCompte() {
+    this.buttonModifierCompte = "invisible";
+    this.buttonAnnulerCompte = "visible";
+    this.buttonSupprimerCompte = "invisible";
+    this.buttonEnregistrerCompte = "visible";
+    this.formCreationCompte = "invisible";
+    
+    this.compteForm.controls['updateDescription'].enable();
+    this.compteForm.controls['updateSolde'].enable();
+  }
+
+  annulerCompte() {
+    this.buttonModifierCompte = "visible";
+    this.buttonAnnulerCompte = "invisible";
+    this.buttonSupprimerCompte = "visible";
+    this.buttonEnregistrerCompte = "invisible";
+    this.formCreationCompte = "visible";
+    
+    this.compteForm.controls['updateDescription'].disable();
+    this.compteForm.controls['updateSolde'].disable();
   }
 }
