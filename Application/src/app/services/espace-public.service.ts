e4409d404f86b17a59b1dbf0df826a19c890b205
Claire <claire.aly1@gmail.com>
2018-07-17 15:54:13 +0200

    Espace Public demande adhesion et message

--- Application/src/app/services/espace-public.service.ts
+++ Application/src/app/services/espace-public.service.ts
@@ -3,17 +3,36 @@ import { Http, Response} from "@angular/http";
 import { Observable } from "rxjs";
 import { DemandeInscription } from '../modeles/demande-inscription';
 import { map, catchError } from "rxjs/operators";
-
+import { MessagePublic } from '../modeles/message-public';
+import { Session } from '../modeles/session'
 
 @Injectable()
 export class EspacePublicService {
 
-	private getConnexionUrl: string = "http://localhost:9090/GestiBankBackEnd/public/adhesion";
+	private getConnexionUrl: string = "http://localhost:9090/GestiBankBackEnd/public";
 
 	constructor(private http: Http) {}
 
-	adhesion(demandeInscription: DemandeInscription): Observable<boolean> {
-		return this.http.post(this.getConnexionUrl, demandeInscription)
+	demandeInscription(demandeInscription: DemandeInscription): Observable<boolean> {
+		return this.http.post(this.getConnexionUrl + "/adhesion", demandeInscription)
+			.pipe(
+				map((res:Response) => res.json()),
+				catchError((error:any) => Observable.throw(error.json().error || "Server error"))
+			);
+	}
+
+
+	createMessagePublic(messagePublic: MessagePublic): Observable<boolean> {
+		return this.http.post(this.getConnexionUrl + "/message", messagePublic)
+			.pipe(
+				map((res:Response) => res.json()),
+				catchError((error:any) => Observable.throw(error.json().error || "Server error"))
+			);
+	}
+
+
+	connexion(connexion: Array<string>): Observable<Session> {
+		return this.http.post(this.getConnexionUrl + "/connexion", connexion)
 			.pipe(
 				map((res:Response) => res.json()),
 				catchError((error:any) => Observable.throw(error.json().error || "Server error"))
